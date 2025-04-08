aa60219d80d7947b6b8843b23b88eb5170f63f9b
José Geovanny Mejía Ramos <jgmejia2@espe.edu.ec>
2025-04-08 14:32:51 -0500

    Mejoramiento del Navbar
    Eliminar la bomba o ovalo dentras de los items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,44 +13,46 @@ const Navbar = () => {
     const isActive = (path) => location.pathname === path;
 
     return (
-        <div className="navbar-wrapper">
-            <nav className="navbar navbar-expand-lg custom-navbar px-3 py-2 shadow">
-                <div className="container-fluid justify-content-between align-items-center">
-                    <Link to="/" className="nav-item-oval d-flex align-items-center gap-2 text-decoration-none">
-                        <img src="/Logo.png" alt="Logo" className="logo" style={{ width: '45px' }} />
-                    </Link>
+        <nav className="navbar navbar-expand-lg fixed-top bg-transparent px-4 py-2">
+            <div className="container-fluid d-flex justify-content-between align-items-center">
+                <Link to="/" className="logo-capsule text-decoration-none">
+                    <img src="/Logo.png" alt="Logo" className="logo" style={{ width: '45px' }} />
+                </Link>
+                <button className="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                    <button className="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                        <ul className="navbar-nav gap-3">
-                            <li className="nav-item">
-                                <Link to="/" className={`nav-item-oval nav-link ${isActive('/') ? 'active-link' : ''}`}>
-                                    <FontAwesomeIcon icon={faHome} /> Inicio
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/menu" className={`nav-item-oval nav-link ${isActive('/menu') ? 'active-link' : ''}`}>
-                                    <FontAwesomeIcon icon={faUtensils} /> Menú
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/contacto" className={`nav-item-oval nav-link ${isActive('/contacto') ? 'active-link' : ''}`}>
-                                    <FontAwesomeIcon icon={faPhone} /> Contacto
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <a href="https://www.facebook.com/profile.php?id=100076219538221" target="_blank" rel="noopener noreferrer" className="nav-item-oval nav-link">
-                                    <FontAwesomeIcon icon={faFacebook} /> Facebook
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav gap-3">
+                        <li className="nav-item">
+                            <Link to="/" className={`nav-item-oval nav-link ${isActive('/') ? 'active-link' : ''}`}>
+                                <FontAwesomeIcon icon={faHome} /> Inicio
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/menu" className={`nav-item-oval nav-link ${isActive('/menu') ? 'active-link' : ''}`}>
+                                <FontAwesomeIcon icon={faUtensils} /> Menú
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contacto" className={`nav-item-oval nav-link ${isActive('/contacto') ? 'active-link' : ''}`}>
+                                <FontAwesomeIcon icon={faPhone} /> Contacto
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                href="https://www.facebook.com/profile.php?id=100076219538221"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="nav-item-oval nav-link"
+                            >
+                                <FontAwesomeIcon icon={faFacebook} /> Facebook
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 };
 
